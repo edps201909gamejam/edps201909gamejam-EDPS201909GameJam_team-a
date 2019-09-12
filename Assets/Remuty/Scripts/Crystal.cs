27c326221c64d7d13a48c89e9feecae3aa6745a1
れむてぃ <>
2019-09-12 23:24:22 +0900

    破壊可能オブジェクトにHPゲージ追加

--- Assets/Remuty/Scripts/Crystal.cs
+++ Assets/Remuty/Scripts/Crystal.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Crystal : MonoBehaviour
 {
     [SerializeField] GameObject[] monster;
     float interval = 2;
+    public GameObject gauge;
+    float hp;
+    public Status status;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hp = status.maxHp;
     }
 
     // Update is called once per frame
@@ -22,5 +26,23 @@ public class Crystal : MonoBehaviour
             Instantiate(monster[m], transform.position, Quaternion.identity);
             interval = 2;
         }
+        GaugeDown(hp, status.maxHp);
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+	private void OnCollisionStay(Collision c)
+    {
+        if (c.gameObject.tag == "Braver")
+        {
+            int braver_atk = c.gameObject.GetComponent<Braver>().status.atk;
+            hp -= braver_atk * Time.deltaTime;
+        }
+    }
+    void GaugeDown(float current, int max)
+    {
+        gauge.GetComponent<Image>().fillAmount = current / max;
     }
 }
